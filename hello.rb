641bf1f628bf7cb44fae1787a982c774b5721208
Hakan Ayhan <hakanayhnn@gmail.com>
2021-08-22 19:37:32 +0300

    finished the calculator can make 4 math operations"

--- hello.rb
+++ hello.rb
@@ -1,3 +1,24 @@
-puts "Please enter your name"
-name = gets.chomp()
-puts ("Hello "+ name)
+puts "Calculator"
+puts "What math operation do you want to do?"
+puts "Please enter 1 to addition, 2 to subtraction, 3 to multiplication, 4 to division"
+operation = gets.chomp().to_i
+puts "Please enter the first number"
+number1 = gets.chomp().to_i
+puts "Please enter the second number"
+number2 = gets.chomp().to_i
+
+if operation == 1
+  op = "+"
+  result = number1 + number2
+elsif operation == 2
+  op = "-"
+  result = number1 - number2
+elsif operation == 3
+  op = "x"
+  result = number1 * number2
+elsif operation == 4
+  op = "/"
+  result = number1 / number2
+end
+
+puts (number1.to_s + op + number2.to_s + "=" + result.to_s)
